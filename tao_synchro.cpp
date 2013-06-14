a72e83ed69a990451cd56d74ade4ac1520d4120f
Baptiste Soulisse <baptiste.soulisse@taodyne.com>
2013-06-14 15:01:08 +0200

    Merge branch 'master' of ssh://nano.taodyne.com/~ddd/Work/tao into states
    Conflicts:
    	modules/chroma_key
    	modules/mapping/cube_map.cpp
    	modules/mapping/plane.cpp
    	modules/mapping/texture_mapping.cpp
    	modules/materials/material.cpp
    	modules/shading/shading.cpp
    	modules/slideshow_3d/slideshow_3d2.xl
    	modules/vlc_audio_video
    	modules/water_surface/surface.cpp
    	modules/water_surface/water.cpp
    	tao/module_manager.h
    	tao/tao.pro
    	tao/tao_main.cpp
    	tao/widget.cpp

--- tao_synchro.cpp
+++ tao_synchro.cpp
@@ -87,10 +87,7 @@ int module_init(const Tao::ModuleApi *api, const Tao::ModuleInfo *)
     XL_INIT_TRACES();
 
     synchroBasic::tao = api;
-//    if (api->checkImpressOrLicense("TaoSynchro 1.0"))
-        return no_error;
-
-//    return error_invalid_license;
+    return no_error;
 }
 
 
